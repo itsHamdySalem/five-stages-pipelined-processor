3e856693e92ba749b81caac4131a189347f8a2d6
abotaha23 <at77031@gmail.com>
2023-12-05 20:30:17 +0200

    fixed and tested the register file

--- src/ALU/RegistersFile.vhd
+++ src/ALU/RegistersFile.vhd
@@ -9,29 +9,34 @@ entity RegistersFile is
            Rsrc2_sel : in std_logic_vector(2 downto 0);
            write_flag : in std_logic;
            write_reg : in std_logic_vector(2 downto 0);
-           write_data : in STD_LOGIC_VECTOR(15 downto 0);
-           readData1 : out STD_LOGIC_VECTOR(15 downto 0);
-           readData2 : out STD_LOGIC_VECTOR(15 downto 0)
+           write_data : in STD_LOGIC_VECTOR(31 downto 0);
+           readData1 : out STD_LOGIC_VECTOR(31 downto 0);
+           readData2 : out STD_LOGIC_VECTOR(31 downto 0)
     );
 end RegistersFile;
 
 architecture Behavioral of RegistersFile is
     type registerFile is array (0 to 7) of STD_LOGIC_VECTOR(31 downto 0);
     signal R : registerFile := (others => (others => '0'));
-    signal PC, SP : STD_LOGIC_VECTOR(31 downto 0);
-    signal CCR : STD_LOGIC_VECTOR(3 downto 0);
 begin
     process(clk, rst)
     begin
+        -- R(0) <= "00000000000000000000000000000001";
+        -- R(1) <= "00000000000000000000000000000010";
+        -- R(2) <= "00000000000000000000000000000100";
+        -- R(3) <= "00000000000000000000000000001000";
+        -- R(4) <= "00000000000000000000000000010000";
+        -- R(5) <= "00000000000000000000000000100000";
+        -- R(6) <= "00000000000000000000000001000000";
+        -- R(7) <= "00000000000000000000000010000000";
         if rst = '1' then
             R <= (others => (others => '0'));
-        elsif rising_edge(clk) then
+        elsif falling_edge(clk) then
             if write_flag = '1' then
                 R(to_integer(unsigned(write_reg))) <= write_data;
             end if;
-        else 
-            readData1 <= R(to_integer(unsigned(Rsrc1_sel)));
-            readData2 <= R(to_integer(unsigned(Rsrc2_sel)));
         end if;
     end process;
+    readData1 <= R(to_integer(unsigned(Rsrc1_sel)));
+    readData2 <= R(to_integer(unsigned(Rsrc2_sel)));
 end Behavioral;
