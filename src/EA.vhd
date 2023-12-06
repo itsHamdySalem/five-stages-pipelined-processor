d9130b4fb754212d7136eb472773db456582f836
ahmedibrahim404 <ahmedie20022011@gmail.com>
2023-12-06 22:29:38 +0200

    ADD EA

---
+++ src/EA.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+USE IEEE.numeric_std.all;
+
+entity EA is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+            EA_in : in STD_LOGIC_VECTOR(19 downto 0);
+
+            EA_out : out STD_LOGIC_VECTOR(19 downto 0)
+           );
+end EA;
+
+architecture Behavioral of EA is
+
+begin
+    process(clk, rst) -- TODO:: deal with the reset if there is any.
+    begin 
+        if falling_edge(clk) then
+            EA_out <= EA_in;
+        end if;
+    end process;
+
+end Behavioral;
---
+++ src/EAHandler.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+USE IEEE.numeric_std.all;
+
+entity EA_Handler_Unit is
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+            instruction1 : in STD_LOGIC_VECTOR(15 downto 0);
+            instruction2 : in STD_LOGIC_VECTOR(15 downto 0);
+            EA : out STD_LOGIC_VECTOR(19 downto 0)
+           );
+end EA_Handler_Unit;
+
+architecture Behavioral of EA_Handler_Unit is
+
+begin
+
+    EA <= instruction1(7 downto 4) & instruction2;
+
+end Behavioral;
